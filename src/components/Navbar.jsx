c2f4282a50d529c3a44eb060b3de57be7d96f511
acastagnino96 <109534721+acastagnino96@users.noreply.github.com>
2023-04-17 18:09:25 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,6 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import CartWidget from './CartWidget';
 import {Link} from 'react-router-dom';
 import "./Navbar.css";
-import { storage } from '../utils/firebaseConfig.js';
 
 function BasicExample() {
   return (
